88201e91a133e01e13f3602cf20c71a71288625f
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-06-19 22:49:55 -0300

    implementando css

--- scriptCEP.js
+++ scriptCEP.js
@@ -1,1 +1,9 @@
 'use strict';
+
+
+
+const cep = document.getElementById("recebe-cep").value;
+const url = `viacep.com.br/ws/${cep}/json/`;
+document.getElementById("busca-cep")
+        .addEventListener('onClick',pesquisarCep);
+        
