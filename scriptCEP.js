22d740b7925ba15ed3d767fe878adf820c582c00
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-06-20 21:00:33 -0300

    add css

--- scriptCEP.js
+++ scriptCEP.js
@@ -1,9 +1,22 @@
 'use strict';
 
 
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
 
 const cep = document.getElementById("recebe-cep").value;
 const url = `viacep.com.br/ws/${cep}/json/`;
+
+function mudaBorder() { 
+   document.getElementById("recebe-cep").Style.border.Color = '#008CBA';
+  
+}
+
+document.getElementById("recebe-cep")
+    .addEventListener('onFocus', mudaBorder());
 document.getElementById("busca-cep")
-        .addEventListener('onClick',pesquisarCep);
-        
+    .addEventListener('onClick', pesquisarCep);
