8edeba51ec2a9a66ecb8948cc63a25134a4355ea
VictorMacabu <54696589+VictorMacabu@users.noreply.github.com>
2021-06-22 01:05:53 -0300

    logica e tratamento dos valores recebidos

--- scriptCEP.js
+++ scriptCEP.js
@@ -1,21 +1,42 @@
 'use strict';
+const eNumero = (numero) => /^[0-9]+$/.test(numero);// .test verifica se o input é número 
 
-const buscarCep = async()=> {
+const cepValido = (cep) => cep.length == 8 && eNumero(cep); // verifica se o input tem 8 digitos
+
+const buscarCep = async () => { //Requisita à API e trata o cep recebido  
     const cep = document.getElementById("recebe-cep").value;
     const url = `http://viacep.com.br/ws/${cep}/json/`;
-    
-    const dados = await fetch(url);
-    const valores  = await dados.json();
-    console.log(valores.json());
+    if (cepValido(cep)) { //verifica se cep pode ser valido
+
+        const dados = await fetch(url);
+        const valores = await dados.json();
+
+        if (valores.hasOwnProperty('erro')) { //verifica se viacep retorna json com erro=true.
+            document.getElementById('recebe-cep').value = 'CEP não encontrado!';
+            document.getElementById('recebe-cep').style.color = "red";
+            document.getElementById('recebe-cep').style.outlineColor = "red";
+            document.getElementById("iRespostaCEP").style.display = "none"; //muda o estado do display do mostrador 
+        } else {
+            document.getElementById('recebe-cep').style.outlineColor = "#6aa4f8";
+            atribuirValores(valores);
+            document.getElementById("iRespostaCEP").style.display = ""; //muda o estado do display do mostrador 
+        }
+    } else {
+        document.getElementById('recebe-cep').value = 'CEP incorreto!';
+        document.getElementById('recebe-cep').style.color = "red";
+        document.getElementById('recebe-cep').style.outlineColor = "red";
+        document.getElementById("iRespostaCEP").style.display = "none"; //muda o estado do display do mostrador 
+    }
+
 }
 
-document.getElementById("busca-cep")
+document.getElementById("busca-cep") //cria evento click no buscador de CEP
     .addEventListener("click", buscarCep);
- 
 
-    const preencherFormulario = (valores) => {
-        document.getElementById('endereco').value = endereco.logradouro;
-        document.getElementById('bairro').value = endereco.bairro;
-        document.getElementById('cidade').value = endereco.localidade;
-        document.getElementById('estado').value = endereco.uf;
-    }
+const atribuirValores = (valores) => { //insere os valores extraidos no mostrador.
+    document.getElementById('cep').innerHTML = valores.cep;
+    document.getElementById('logradouro').innerHTML = valores.logradouro;
+    document.getElementById('estado').innerHTML = valores.uf;
+    document.getElementById('cidade').innerHTML = valores.localidade;
+
+}
